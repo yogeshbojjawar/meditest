cde65eb9d13783e8be86967b16df101c2eecf22c
yogesh <yogeshbojjawar97@gmail,com>
2024-02-06 23:20:27 +0530

    first comit

---
+++ flutter/src/flutter/flutst.java
@@ -0,0 +1,10 @@
+package flutter;
+
+public class flutst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ flutter/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Yogesh
+ *
+ */
+module flutter {
+}
